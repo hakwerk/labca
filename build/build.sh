3116c85c2cb0e0805090b470c78c2f2c8737d140
Arjan H <github@hakwerk.com>
2025-01-12 11:43:41 +0100

    Bump boulder version to release-2025-01-06

--- build/build.sh
+++ build/build.sh
@@ -8,7 +8,7 @@ TMP_DIR=$(pwd)/tmp
 rm -rf $TMP_DIR && mkdir -p $TMP_DIR/{admin,bin,logs,src}
 
 boulderDir=$TMP_DIR/src
-boulderTag="release-2024-12-10"
+boulderTag="release-2025-01-06"
 boulderUrl="https://github.com/letsencrypt/boulder/"
 cloneDir=$(pwd)/..
 
